2b09242582240eb3e3e5c8cdf74d20158fe6ec12
ss-yusuke-aws <y-aoki@sproutly.co.jp>
2022-02-07 11:45:58 +0000

    ruby-1 完了

---
+++ ruby-1/ruby1.rb
@@ -0,0 +1,39 @@
+# 動的型付け言語
+# Java
+## int num = 42;
+# ruby
+num = 42
+
+class String
+    def hello
+        puts "Hello"
+    end
+end
+
+# データ型
+## 変数宣言
+## この変数にはhogehoge種類のデータを格納する
+
+# int num = 1
+# int num2 = 2
+# num + num2 = 3
+# string 12
+
+データ型
+1. 文字列(string)
+string = "hogehoge!"
+2. 数値(int)
+num = 4
+3. 論理型(boolean)
+YES or No
+ON or OFF
+TRUE or False
+4. 日付型
+2021/1/23-14:00:00
+5. バイナリ型
+画像
+6. 配列型
+array = []
+array = [1,2,3]
+array = ["test","tete"]
+7. オブジェクト型
