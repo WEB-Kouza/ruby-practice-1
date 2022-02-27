2bb1194ac55dd0ce5c7a7aceecca011b1fba717f
ss-yusuke-aws <y-aoki@sproutly.co.jp>
2022-02-27 16:19:38 +0000

    hash

---
+++ ruby-5/hash.rb
@@ -0,0 +1,21 @@
+date = {
+    "name":"yu",
+    "gender":"male",
+    "age":46
+}
+
+date = ["yu","male","age"]
+
+puts date
+
+#JSON 
+# {
+#     "user":{
+#         "name":"yu",
+#         "age":43
+#     }
+# }
+
+puts date[:name]
+puts date[:gender]
+puts date[:age]
